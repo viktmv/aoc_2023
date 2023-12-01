f2295a646c5bc6f127e295d2a12183ac8f90ac01
Viktor T <vtymofeev@tucows.com>
2023-12-01 14:25:36 -0500

    Tiny refactor [2]

--- day1/run.go
+++ day1/run.go
@@ -8,6 +8,18 @@ import (
 	"strconv"
 )
 
+var numbers = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
 var filename string = "input.txt"
 
 func main() {
@@ -38,18 +50,6 @@ func main() {
 	}
 
 	fmt.Println("Result: ", result)
-}
-
-var numbers = map[string]string{
-	"one":   "1",
-	"two":   "2",
-	"three": "3",
-	"four":  "4",
-	"five":  "5",
-	"six":   "6",
-	"seven": "7",
-	"eight": "8",
-	"nine":  "9",
 }
 
 func ReadLine(line string) int {
